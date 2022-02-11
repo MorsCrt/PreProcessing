9b75ded90f2a4021cfbe624ef61b4a9b77da029a
Yunus Emre Çoban <91698352+MorsCrt@users.noreply.github.com>
2022-02-11 09:35:21 -0800

    Add files via upload
    Label Encoder, First Optimize

---
+++ labelencoder.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from datetime import datetime
+
+df = pd.read_csv("eksikveriler.csv")
+
+
+def labelencoder(dataframe):
+    """
+    :param dataframe:
+    :return: encoded dataframe
+    """
+    cat_colname = dataframe.select_dtypes(
+        include=['object']).columns.values
+    cat_uniq_colname = [col for col in dataframe[cat_colname]
+                        if (dataframe[col].nunique() == 3)]
+    unique_rows = np.unique(np.array(dataframe[cat_uniq_colname].values))
+
+    ohe = preprocessing.OneHotEncoder()
+    col_trans = ohe.fit_transform(dataframe[cat_uniq_colname]).toarray()
+    ohe_col = ohe.fit_transform(dataframe[cat_uniq_colname]).toarray()
+
+    dataframe_encod = pd.DataFrame(data=col_trans,
+                                   index=range(len(ohe_col)),
+                                   columns=unique_rows)
+    dataframe = dataframe.drop(cat_uniq_colname, axis=1)
+    dataframe = pd.concat([dataframe, dataframe_encod], axis=1)  # Concat mean filled table and dropped dataframe
+
+    return dataframe
+
+
+start_time = datetime.now()
+x = labelencoder(df)
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
