964b971e1a10cc2d6c7f1a4108b85fbf4189283e
Yunus Emre Çoban <91698352+MorsCrt@users.noreply.github.com>
2022-03-10 01:41:19 -0800

    Update valmeanfiller.py

--- valmeanfiller.py
+++ valmeanfiller.py
@@ -3,38 +3,41 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from datetime import datetime
 
-df = pd.read_csv("eksikveriler.csv")
-
+df = pd.read_csv("Multi-Gender-done.csv",low_memory=False)
 
 def valmeanfiller(dataframe):
-    global df
     # missval_colname = Which columns have missing values and numerical
-    missval_colname = np.array([col for col in df.columns
-                                if df[col].isnull().any()])
-    numeric_colname = df.select_dtypes(
-        include=['float64', 'int64']).columns.values
+    missval_colname = np.array([col for col in dataframe.columns
+                                if dataframe[col].isnull().any()])
+    numeric_colname = dataframe.select_dtypes(
+        include=['float64',
+                 'float32',
+                 'int64',
+                 'int32']).columns.values
     # Intersection of the numeric_colname and missval_colname
-    inter_colnames = np.intersect1d(missval_colname, numeric_colname)
+    inter_colnames = np.intersect1d(missval_colname,
+                                    numeric_colname)
     # Saving order name for after concat
-    all_col_name = df.columns.values
 
     imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
     df_misscol = dataframe[inter_colnames].values  # List to dataframe
     imputer = imputer.fit(df_misscol)
     df_misscol = imputer.transform(df_misscol)
 
+    df = dataframe.drop(inter_colnames, axis=1)  # Drop columns(have missing values) on the main dataframe
+
     df_filled = pd.DataFrame(data=df_misscol,
                              index=range(len(df_misscol)),
                              columns=inter_colnames)
 
-    df = df.drop(inter_colnames, axis=1)  # Drop columns(have missing values) on the main dataframe
+
     df = pd.concat([df, df_filled], axis=1)  # Concat mean filled table and dropped dataframe
-    df = df[all_col_name]
 
     return df
 
 
 start_time = datetime.now()
-x = valmeanfiller(df)
+df_meanfilled = valmeanfiller(df)
 end_time = datetime.now()
+df_meanfilled.to_csv("alldone.csv",index=False)
 print('Duration: {}'.format(end_time - start_time))
